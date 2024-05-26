21460a80108a969d9cdc3c2d2a4b4a45317d6b1b
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-05-27 01:34:13 +0530

    initial commit

---
+++ Backend/ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+
+        public async Task joinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.userName} has joined");
+        }
+
+        public async Task joinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.chatRoom);
+            await Clients.Group(conn.chatRoom).SendAsync("ReceiveMessage","admin",$"{conn.userName} has joined {conn.chatRoom}");
+        }
+    }
+}
